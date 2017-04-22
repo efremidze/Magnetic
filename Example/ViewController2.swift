2b5e5fcc890f90da123cfd9e207f63de161492d7
Lasha Efremidze <efremidzel@hotmail.com>
2017-04-22 14:53:02 -0700

    added example

---
+++ Example/ViewController2.swift
@@ -0,0 +1,102 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Lasha Efremidze on 3/8/17.
+//  Copyright © 2017 efremidze. All rights reserved.
+//
+
+import SpriteKit
+import Magnetic
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var magneticView: MagneticView! {
+        didSet {
+            magnetic.magneticDelegate = self
+            #if DEBUG
+                magneticView.showsFPS = true
+                magneticView.showsDrawCount = true
+                magneticView.showsQuadCount = true
+            #endif
+        }
+    }
+    
+    var magnetic: Magnetic {
+        return magneticView.magnetic
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for _ in 0..<12 {
+            add(nil)
+        }
+    }
+    
+    @IBAction func add(_ sender: UIControl?) {
+        let name = UIImage.names.randomItem()
+        let color = UIColor.colors.randomItem()
+        let node = CustomNode(text: name.capitalized, image: UIImage(named: name), color: color, radius: 40)
+        magnetic.addChild(node)
+    }
+    
+    @IBAction func reset(_ sender: UIControl?) {
+        let speed = magnetic.physicsWorld.speed
+        magnetic.physicsWorld.speed = 0
+        let sortedNodes = magnetic.children.flatMap { $0 as? Node }.sorted { node, nextNode in
+            let distance = node.position.distance(from: magnetic.magneticField.position)
+            let nextDistance = nextNode.position.distance(from: magnetic.magneticField.position)
+            return distance < nextDistance && node.isSelected
+        }
+        var actions = [SKAction]()
+        for (index, node) in sortedNodes.enumerated() {
+            node.physicsBody = nil
+            let action = SKAction.run { [unowned magnetic, unowned node] in
+                if node.isSelected {
+                    let point = CGPoint(x: magnetic.size.width / 2, y: magnetic.size.height + 40)
+                    let movingXAction = SKAction.moveTo(x: point.x, duration: 0.2)
+                    let movingYAction = SKAction.moveTo(y: point.y, duration: 0.4)
+                    let resize = SKAction.scale(to: 0.3, duration: 0.4)
+                    let throwAction = SKAction.group([movingXAction, movingYAction, resize])
+                    node.run(throwAction) { [unowned node] in
+                        node.removeFromParent()
+                    }
+                } else {
+                    node.removeFromParent()
+                }
+            }
+            actions.append(action)
+            let delay = SKAction.wait(forDuration: TimeInterval(index) * 0.002)
+            actions.append(delay)
+        }
+        magnetic.run(.sequence(actions)) { [unowned magnetic] in
+            magnetic.physicsWorld.speed = speed
+        }
+    }
+    
+}
+
+// MARK: - MagneticDelegate
+extension ViewController: MagneticDelegate {
+    
+    func magnetic(_ magnetic: Magnetic, didSelect node: Node) {
+        print("didSelect -> \(node)")
+    }
+    
+    func magnetic(_ magnetic: Magnetic, didDeselect node: Node) {
+        print("didDeselect -> \(node)")
+    }
+    
+}
+
+class CustomNode: Node {
+    override var image: UIImage? {
+        didSet {
+            guard let image = image else { return }
+            sprite.texture = SKTexture(image: image)
+        }
+    }
+    override func selectedAnimation() {}
+    override func deselectedAnimation() {}
+}
