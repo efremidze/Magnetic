87b1d47c95b678280201713297af3aad962cc83a
Lasha Efremidze <lasha.efremidze@ring.com>
2017-05-11 18:35:49 -0700

    Added SKMultilineLabelNode

---
+++ Sources/SKMultilineLabelNode.swift
@@ -0,0 +1,88 @@
+//
+//  SKMultilineLabelNode.swift
+//  Magnetic
+//
+//  Created by Lasha Efremidze on 5/11/17.
+//  Copyright © 2017 efremidze. All rights reserved.
+//
+
+import SpriteKit
+
+open class SKMultilineLabelNode: SKNode {
+    
+    open var text: String? { didSet { update() } }
+    
+    open var fontName: String? { didSet { update() } }
+    open var fontSize: CGFloat = 32 { didSet { update() } }
+    open var fontColor: UIColor? { didSet { update() } }
+    
+    open var verticalAlignmentMode: SKLabelVerticalAlignmentMode = .baseline { didSet { update() } }
+    open var horizontalAlignmentMode: SKLabelHorizontalAlignmentMode = .center { didSet { update() } }
+    
+    open var separator: String? { didSet { update() } }
+    
+    open var width: CGFloat! { didSet { update() } }
+    
+    func update() {
+        self.removeAllChildren()
+        
+        guard let text = text else { return }
+        
+        print(width)
+        
+        var sizingLabel = SKLabelNode()
+        var stack = Stack<String>()
+        
+        let words = separator.map { text.components(separatedBy: $0) } ?? text.characters.map { String($0) }
+        print(words)
+        for word in words {
+            sizingLabel.append(word)
+            if sizingLabel.frame.width > width {
+                stack.add(toStack: word)
+                sizingLabel = SKLabelNode()
+            } else {
+                stack.add(toCurrent: word)
+            }
+        }
+        
+        print("---> 1")
+        print(stack.values)
+        let lines = stack.values.map { $0.joined(separator: "") }
+        print(lines)
+        for (index, line) in lines.enumerated() {
+            let label = SKLabelNode(fontNamed: fontName)
+            label.text = line
+            label.fontSize = fontSize
+            label.fontColor = fontColor
+            label.verticalAlignmentMode = verticalAlignmentMode
+            label.horizontalAlignmentMode = horizontalAlignmentMode
+            label.position = CGPoint(x: 0, y: (CGFloat(index) * -fontSize))
+            self.addChild(label)
+            print("---> 2")
+        }
+    }
+    
+}
+
+private struct Stack<U> {
+    typealias T = (stack: [[U]], current: [U])
+    private var value: T
+    var values: [[U]] {
+        return value.stack + [value.current]
+    }
+    init() {
+        self.value = (stack: [], current: [])
+    }
+    mutating func add(toStack element: U) {
+        self.value = (stack: value.stack + [value.current], current: [element])
+    }
+    mutating func add(toCurrent element: U) {
+        self.value = (stack: value.stack, current: value.current + [element])
+    }
+}
+
+private extension SKLabelNode {
+    func append(_ text: String) {
+        self.text = (self.text ?? "") + text
+    }
+}
