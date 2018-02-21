5d60e6d9084e391e2e39e1ed78478e0c159e8020
bj97301 <bj97301@gmail.com>
2018-02-21 13:42:14 -0800

    enable single node movements

--- Sources/Magnetic.swift
+++ Sources/Magnetic.swift
@@ -28,8 +28,28 @@ open class Magnetic: SKScene {
      Controls whether you can select multiple nodes.
      */
     open var allowsMultipleSelection: Bool = true
+
+    /**
+     Lets the user move individule nodes.
+     **/
+    open var allowSingleNodeMovement: Bool = false
+    
+    /**
+     Lets the user move all of the nodes at once.  On by default.
+     **/
+    open var allowAllNodeMovement: Bool = true
+    
+    /**
+     The amount of distance the user's finger is able to travle before considering it a move event instead of a selection.
+     **/
+    open var nodeSelectionForgivenessDistance: CGFloat = 5
+    
     
     var isDragging: Bool = false
+    var movingNode: SKNode? = nil
+    var movingPreviousDampingValue: CGFloat? = nil
+    var initialTouchLocation: CGPoint? = nil
+    var initialTouchStartedOnNode: Bool = false
     
     /**
      The selected children.
@@ -104,43 +124,84 @@ extension Magnetic {
     override open func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first {
             let location = touch.location(in: self)
-            let previous = touch.previousLocation(in: self)
-            
-            if location.distance(from: previous) == 0 { return }
-            
-            isDragging = true
-            
-            let x = location.x - previous.x
-            let y = location.y - previous.y
-            
-            for node in children {
-                let distance = node.position.distance(from: location)
-                let acceleration: CGFloat = 3 * pow(distance, 1/2)
-                let direction = CGVector(dx: x * acceleration, dy: y * acceleration)
-                node.physicsBody?.applyForce(direction)
+            if movingNode == nil &&
+                allowSingleNodeMovement &&
+                initialTouchLocation == nil{
+                for node in children {
+                    let nodeTouchPoint=node.convert(location, to: node)
+                    if node.frame.contains(nodeTouchPoint)
+                    {
+                        movingNode = node
+                        movingPreviousDampingValue = movingNode?.physicsBody?.linearDamping
+                        movingNode?.physicsBody?.linearDamping = CGFloat.greatestFiniteMagnitude
+                        if initialTouchLocation == nil{
+                            initialTouchStartedOnNode = true
+                        }
+                        break
+                    }
+                }
+            }
+            if !isDragging{
+                isDragging = true
+                initialTouchLocation = location
+            }
+            if allowSingleNodeMovement && initialTouchStartedOnNode, let nodeToMove = movingNode{
+                var safeY = location.y
+                if location.y > self.frame.maxY{
+                    safeY = self.frame.maxY
+                }else if location.y < self.frame.minY{
+                    safeY = self.frame.minY
+                }
+                
+                let safePoint = CGPoint(x: location.x, y: safeY)
+                let convertedSafePoint = nodeToMove.convert(safePoint, to: nodeToMove)
+                nodeToMove.run(.move(to: convertedSafePoint, duration: 0))
+            } else if allowAllNodeMovement{
+                let previous = touch.previousLocation(in: self)
+                if location.distance(from: previous) == 0 { return }
+                
+                let x = location.x - previous.x
+                let y = location.y - previous.y
+                
+                for node in children {
+                    let distance = node.position.distance(from: location)
+                    let acceleration: CGFloat = 3 * pow(distance, 1/2)
+                    let direction = CGVector(dx: x * acceleration, dy: y * acceleration)
+                    node.physicsBody?.applyForce(direction)
+                }
             }
         }
     }
     
     override open func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if
-            !isDragging,
-            let point = touches.first?.location(in: self),
-            let node = nodes(at: point).flatMap({ $0 as? Node }).filter({ $0.path!.contains(convert(point, to: $0)) }).first
-        {
-            if node.isSelected {
-                node.isSelected = false
-                magneticDelegate?.magnetic(self, didDeselect: node)
-            } else {
-                if !allowsMultipleSelection, let selectedNode = selectedChildren.first {
-                    selectedNode.isSelected = false
-                    magneticDelegate?.magnetic(self, didDeselect: selectedNode)
+        if movingNode != nil && movingPreviousDampingValue != nil{
+            movingNode?.physicsBody?.linearDamping = movingPreviousDampingValue!
+        }
+        movingNode = nil
+        movingPreviousDampingValue = nil
+        if let touch = touches.first, let initialLocation = initialTouchLocation{
+            let point = touch.location(in: self)
+            let shouldAllowSelection = (!isDragging || (initialLocation.distance(from: point) < nodeSelectionForgivenessDistance))
+            
+            if shouldAllowSelection,
+                let node = nodes(at: point).flatMap({ $0 as? Node }).filter({ $0.path!.contains(convert(point, to: $0)) }).first
+            {
+                if node.isSelected {
+                    node.isSelected = false
+                    magneticDelegate?.magnetic(self, didDeselect: node)
+                } else {
+                    if !allowsMultipleSelection, let selectedNode = selectedChildren.first {
+                        selectedNode.isSelected = false
+                        magneticDelegate?.magnetic(self, didDeselect: selectedNode)
+                    }
+                    node.isSelected = true
+                    magneticDelegate?.magnetic(self, didSelect: node)
                 }
-                node.isSelected = true
-                magneticDelegate?.magnetic(self, didSelect: node)
             }
         }
         isDragging = false
+        initialTouchLocation = nil
+        initialTouchStartedOnNode = false
     }
     
     override open func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
