46e39eb287ea416053c8afa2e3b83392866c68a9
Lasha Efremidze <efremidzel@hotmail.com>
2020-06-16 23:22:11 -0700

    Added Reset Function to MagneticView, Enabled Mac Catalyst on Example, Refactoring (#94)

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -50,37 +50,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func reset(_ sender: UIControl?) {
-        let speed = magnetic.physicsWorld.speed
-        magnetic.physicsWorld.speed = 0
-        let sortedNodes = magnetic.children.compactMap { $0 as? Node }.sorted { node, nextNode in
-            let distance = node.position.distance(from: magnetic.magneticField.position)
-            let nextDistance = nextNode.position.distance(from: magnetic.magneticField.position)
-            return distance < nextDistance && node.isSelected
-        }
-        var actions = [SKAction]()
-        for (index, node) in sortedNodes.enumerated() {
-            node.physicsBody = nil
-            let action = SKAction.run { [unowned magnetic, unowned node] in
-                if node.isSelected {
-                    let point = CGPoint(x: magnetic.size.width / 2, y: magnetic.size.height + 40)
-                    let movingXAction = SKAction.moveTo(x: point.x, duration: 0.2)
-                    let movingYAction = SKAction.moveTo(y: point.y, duration: 0.4)
-                    let resize = SKAction.scale(to: 0.3, duration: 0.4)
-                    let throwAction = SKAction.group([movingXAction, movingYAction, resize])
-                    node.run(throwAction) { [unowned node] in
-                        node.removeFromParent()
-                    }
-                } else {
-                    node.removeFromParent()
-                }
-            }
-            actions.append(action)
-            let delay = SKAction.wait(forDuration: TimeInterval(index) * 0.002)
-            actions.append(delay)
-        }
-        magnetic.run(.sequence(actions)) { [unowned magnetic] in
-            magnetic.physicsWorld.speed = speed
-        }
+        magneticView.magnetic.reset()
     }
     
 }
