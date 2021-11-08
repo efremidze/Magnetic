6dfeff01fb362a5fbda9bacc3eb0bd26dbc5d47b
Lasha Efremidze <efremidzel@hotmail.com>
2021-11-07 22:53:14 -0800

    Added SwiftUI Support

---
+++ Sources/MagneticView+SwiftUI.swift
@@ -0,0 +1,21 @@
+//
+//  MagneticView+SwiftUI.swift
+//  Magnetic
+//
+//  Created by Lasha Efremidze on 11/7/21.
+//  Copyright © 2021 efremidze. All rights reserved.
+//
+
+// https://www.hackingwithswift.com/quick-start/swiftui/how-to-wrap-a-custom-uiview-for-swiftui
+// https://www.hackingwithswift.com/quick-start/swiftui/how-to-integrate-spritekit-using-spriteview
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+struct MagneticView_SwiftUI: UIViewRepresentable {
+    func makeUIView(context: Context) -> MagneticView {
+        MagneticView()
+    }
+    
+    func updateUIView(_ uiView: MagneticView, context: Context) {}
+}
