3f0793f7f1cad3cb23f6c7ec4d14f87256263cdd
huihuisang <101007168+huihuisang@users.noreply.github.com>
2025-03-15 10:59:25 +0800

    fix: circular reference

--- Sources/MagneticView+Accessibility.swift
+++ Sources/MagneticView+Accessibility.swift
@@ -10,15 +10,16 @@ import UIKit
 
 extension MagneticView {
     func accessibilityCreateSelectionRotor(withName name: String, usingScene magnet: Magnetic) {
-        let selectedRotor = UIAccessibilityCustomRotor(name: name) { predicate in
+        let selectedRotor = UIAccessibilityCustomRotor(name: name) { [weak self] predicate in
+            guard let self = self else { return nil }
             // Sort by Node name/text and ensure there is at least 1 selected Node
-            let selected = magnet.selectedChildren.sorted { $0.text ?? ""  < $1.text ?? "" }
+            let selected = magnet.selectedChildren.sorted { $0.text ?? "" < $1.text ?? "" }
             let all = magnet.children.compactMap { $0 as? Node }
             guard selected.count > 0 else { return nil }
-            
+
             // See which direction the user is scrolling
             let isDirectionForward = predicate.searchDirection == .next
-            
+
             // Get the index of current focused Node
             var currentNodeIndex = isDirectionForward ? all.count : -1
             if
@@ -27,20 +28,20 @@ extension MagneticView {
             {
                 currentNodeIndex = all.firstIndex(of: currentNode) ?? currentNodeIndex
             }
-            
+
             if let node = self.fetchNextSelectedNode(inDirection: isDirectionForward, fromCurrentNodeIndex: currentNodeIndex, inNodeArray: all) {
                 return UIAccessibilityCustomRotorItemResult(targetElement: node, targetRange: nil)
             } else { return nil }
         }
-        
+
         accessibilityCustomRotors = [selectedRotor]
     }
-    
+
     func fetchNextSelectedNode(inDirection forwards: Bool, fromCurrentNodeIndex index: Int, inNodeArray all: [Node]) -> Node? {
         // A closure used to update the while loop
         let nextSearchNode = { forwards ? $0 - 1 : $0 + 1 }
         var searchNode = nextSearchNode(index)
-        while searchNode >= 0 && searchNode < all.count {
+        while searchNode >= 0, searchNode < all.count {
             defer { searchNode = nextSearchNode(searchNode) }
             if all[searchNode].isSelected {
                 return all[searchNode]
